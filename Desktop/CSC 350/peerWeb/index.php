40cd7b8cf23ff2003c2df75e9bdc049447f3b104
hananeDev84 <gbadhanane@gmail.com>
2020-03-24 22:24:32 -0400

    hanane84

--- Desktop/CSC 350/peerWeb/index.php
+++ Desktop/CSC 350/peerWeb/index.php
@@ -20,7 +20,7 @@ require 'config.php';
      <label><b> Username</b></label><br>
       <input name="username" type="text" class="inputvalue" placeholder="Enter username" required/><br>
       <label><b> Password</b></label><br>
-      <input name="password"type="text" class="inputvalue" placeholder="Enter password" required/><br>
+      <input name="password"type="password" class="inputvalue" placeholder="Enter password" required/><br>
       <input name="login" type="submit" id="login-btn" value ="login"/><br>
      <a href="register.php"> <input type="button" id="register-btn" value ="Register"/></a>
 </form>
@@ -29,18 +29,35 @@ require 'config.php';
   {
     $username=$_POST['username'];
     $password=$_POST['password'];
-    $query= "SELECT * from user WHERE username='$username' AND password='$password'";
+    //filter the variable for security
+    //$username = strip_tags(mysqli_real_escape_string($con, trim($username)));
+    //$password = strip_tags(mysqli_real_escape_string($con, trim($password)));
+    
+    $query= "SELECT password from userinfotable WHERE username='$username'";
     $query_run= mysqli_query($con,$query);
+    
     if(mysqli_num_rows($query_run)>0)
     {
       //valid
-      $_SESSION['username']= $username;
-      header('location:homepage.php');
+      $row = mysqli_fetch_array($query_run);
+      $password_hash = $row['password'];
+      //$data = $query->fetch_array();
+      if(password_verify($password, $password_hash))
+       { 
+         print "Password Matched - logging In";
+         $_SESSION['username']= $username;
+         header('location:homepage.php');
+       }
+       else
+       {
+         print "Incorrect Password - Logging Failed";
+       }
     }
     else
     {
-      print "password or user did not exist";
+      print " user did not exist";
     }
+  
   }
 
 
--- Desktop/CSC 350/peerWeb/register.php
+++ Desktop/CSC 350/peerWeb/register.php
@@ -16,26 +16,48 @@ require 'config.php';
     </center>
 
 <form class= "myform" action"register.php" method="post">
+<label><b> Firstname</b></label><br>
+      <input name="firstname" type="text" class="inputvalue" placeholder="Enter your firstname" required/><br>
+      <label><b> Lastname</b></label><br>
+      <input name="lastname" type="text" class="inputvalue" placeholder="Enter your lastname" required/><br>
+     
+      <label><b> Gender</b></label><br>
+      <input name="gender" type="radio" class="radiobtns" value= "male" checked required>Male
+      <input name="gender" type="radio" class="radiobtns" value= "female" required>Female
+      <input name="gender" type="radio" class="radiobtns" value= "other" required>Other<br>
+      <label><b>Level of Ed.</b></label>
+        <select class="qualification" name="qualification">
+           <option value="Associate">Associate</option>
+           <option value="Bachelor">Bachelor</option>
+           <option value="Master">Master</option>
+           <option value="Phd">Phd</option>
+         </select><br>
      <label><b> Username</b></label><br>
       <input name="username" type="text" class="inputvalue" placeholder="Enter username" required/><br>
       <label><b> Password</b></label><br>
-      <input name="password" type="text" class="inputvalue" placeholder="Enter password" required/><br>
+      <input name="password" type="password" class="inputvalue" placeholder="Enter password" required/><br>
       <label><b> Confirm Password</b></label><br>
-      <input name="cpassword" type="text" class="inputvalue" placeholder="Confirm password"required/><br>
+      <input name="cpassword" type="password" class="inputvalue" placeholder="Confirm password"required/><br>
       <input name="submit-btn" type="submit" id="signup-btn" value ="Sign up"/><br>
       <a href= "index.php"><input type="button" id="back-btn" value ="Back"/></a>
       
 </form>
-<?php 
+<?php
    if(isset($_POST['submit-btn']))
    {
       // echo '<script type = "text/javascript"> alert("sign Up button clicked")</script>';
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $gender = $_POST['gender'];
+        $qualification = $_POST['qualification'];
         $username = $_POST['username'];
         $password= $_POST['password'];
         $cpassword= $_POST['cpassword'];
+        $hashedpsw=password_hash($password,PASSWORD_DEFAULT);
+
      if($password==$cpassword)
      {
-         $query= "SELECT* FROM `user` WHERE username ='$username'";
+         $query= "SELECT * FROM userinfotable WHERE username='$username'";
          $query_run= mysqli_query($con,$query);
 
          if(mysqli_num_rows($query_run)>0)
@@ -44,8 +66,8 @@ require 'config.php';
          }
          else
          {
-             $query = "insert into user values('$username', '$password')";
-             $query_run = mysqli_query($con,$query);
+            $query= "insert into userinfotable (username,password,firstname,lastname,gender,qualification) values('$username','$hashedpsw','$firstname','$lastname','$gender','$qualification')";
+            $query_run = mysqli_query($con,$query);
             
              if($query_run)
              {
@@ -63,6 +85,7 @@ require 'config.php';
       }
   
     }
+    
 ?>
 </div>
 </body>
